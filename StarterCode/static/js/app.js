e80cafde33a3249b163df99245d4936a73955113
gwathen2019 <gwathen@att.net>
2019-11-18 15:51:04 -0600

    git commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,17 @@
 // from data.js
 var tableData = data;
+// Use D3 to select the table body
+var tbody = d3.select("tbody");
 
+// Use D3 to select the table
+var table = d3.select("table");
 // YOUR CODE HERE!
+
+data.forEach((UFO_Report) => {
+  var row = tbody.append("tr");
+  Object.entries(UFO_Report).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+console.log(UFO_Report);
+});
