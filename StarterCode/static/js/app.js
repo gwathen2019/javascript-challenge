671455ef1176839b7ea732fad0a980693f81f721
gwathen2019 <gwathen@att.net>
2019-11-19 15:10:15 -0600

    git commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -13,5 +13,24 @@ data.forEach((UFO_Report) => {
     var cell = row.append("td");
     cell.text(value);
   });
-console.log(UFO_Report);
+console.log(UFO_Report)
+});
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  //console.log(tableData);
+
+  var filteredData = tableData.filter(state => tableData.state === inputValue);
+
+  console.log(filteredData)
 });
