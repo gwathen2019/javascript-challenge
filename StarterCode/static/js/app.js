4757af6e9c4349862d831f09ff276e15f5af3f5b
gwathen2019 <gwathen@att.net>
2019-11-20 12:04:16 -0600

    git commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,48 +1,53 @@
 // from data.js
 var tableData = data;
 // Use D3 to select the table body
-
 var tbody = d3.select("tbody");
-
 // Use D3 to select the table
 var table = d3.select("table");
 // YOUR CODE HERE!
-
 data.forEach((UFO_Report) => {
   var row = tbody.append("tr");
   Object.entries(UFO_Report).forEach(([key, value]) => {
     var cell = row.append("td");
     cell.text(value);
   });
-console.log(UFO_Report)
+//console.log(UFO_Report)
 });
 
 // Select the button
 var button = d3.select("#filter-btn");
-
+//var tbody=d3.select("tbody");
 button.on("click", function() {
-
+  tbody.selectAll("tr").remove();
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
-
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
-
   console.log(inputValue);
-  //console.log(tableData);
 
   var filteredData = tableData.filter(ufo_date => ufo_date.datetime === inputValue);
 
   console.log(filteredData)
-//});
-
-  var filtered_table = filteredData;
+});
+/*
+  data.forEach((UFO_Filter_Report) => {
+    var row = tbody.append("tr");
+    Object.entries(UFO_Filter_Report).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  console.log(UFO_Filter_Report)
+  });
+});
+/*
+//button.on("click", function() {
+  //var filtered_table = filteredData;
   // Use D3 to select the table body
 
-  var tbody = d3.select("tbody");
+  //var tbody = d3.select("tbody");
 
   // Use D3 to select the table
-  var table = d3.select("table");
+  //var table = d3.select("filteredData");
   // YOUR CODE HERE!
 
   data.forEach((UFO_Filter_Report) => {
@@ -54,3 +59,4 @@ button.on("click", function() {
   console.log(UFO_Filter_Report)
   });
 });
+*/
